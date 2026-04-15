a05503a461b01ca3a09b7e5d0cc68a77e1305ee8
Prasanna B <prasannab806@gmail.com>
2026-04-15 17:21:42 +0530

    Implement Roman numeral to integer conversion(5th Problem)
    Added Convert class to handle Roman numeral conversion.

--- src/leet_code/RomanToInteger.java
+++ src/leet_code/RomanToInteger.java
@@ -1,5 +1,41 @@
 package leet_code;
 import java.util.*;
+class Convert{
+	public int getValue(char s) {
+		switch(s) {
+		case 'I' :return 1;
+        case 'V': return 5;
+        case 'X': return 10;
+        case 'L': return 50;
+        case 'C': return 100;
+        case 'D': return 500;
+        default: return 1000;
+		}
+	}
+	
+	public int romanToInt(String s) {
+		int total = 0;
+		for(int i=0;i<s.length();i++) {
+			int val = getValue(s.charAt(i));
+			if(i<s.length()-1 && val<getValue(s.charAt(i+1))) {
+				total-=val;
+				
+			}
+			else {
+				total+=val;
+			}
+		}
+		return total;
+		
+	}
+}
 public class RomanToInteger {
+	public static void main(String[] args) {
+	String s = "III";
+	Convert con = new Convert();
+	int val = con.romanToInt(s);
+	System.out.println(val);
+	
+	}
 
 }
