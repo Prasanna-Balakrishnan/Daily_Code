731a72123f0b0c9fe37e03e9b9daa6e3a3c6d361
Prasanna-Balakrishnan <prasannab806@gmail.com>
2026-04-16 19:37:02 +0530

    2nd Problem

---
+++ src/leet_code/MergeTwoSortedList.java
@@ -0,0 +1,43 @@
+package leet_code;
+import java.util.*;
+class ListNode{
+	int val;
+	ListNode next;
+	ListNode(){
+		
+	}
+	ListNode(int val){
+		this.val=val;
+	}
+	ListNode(int val,ListNode next){
+		this.next=next;
+		this.val=val;
+		
+	}
+}
+
+class FindSolution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if (list1 == null) {
+        	return list2;
+        	
+        }
+        if(list2 == null) {
+        	return list1;
+        }
+        if(list1.val < list2.val) {
+        	list1.next= mergeTwoLists(list1.next,list2);
+        	return list1;
+        }
+        else {
+        	list2.next =  mergeTwoLists(list1,list2.next);
+        	return list2;
+        }
+    }
+}
+public class MergeTwoSortedList {
+	public static void main(String[] args) {
+		
+	}
+
+}
