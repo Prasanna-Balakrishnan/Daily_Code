5579899ed3636412d27b46d1ec57755b4e6ea106
Prasanna-Balakrishnan <prasannab806@gmail.com>
2026-04-17 15:12:54 +0530

    7th Problem

---
+++ src/leet_code/ValidParanthesis.java
@@ -0,0 +1,40 @@
+package leet_code;
+
+import java.util.*;
+
+class SolutionCheck {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch : s.toCharArray()) {
+
+            if (ch == '(') stack.push(')');
+            else if (ch == '{') stack.push('}');
+            else if (ch == '[') stack.push(']');
+            else {
+                if (stack.isEmpty() || stack.pop() != ch) {
+                    return false;
+                }
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
+
+public class ValidParanthesis {
+    public static void main(String[] args) {
+        SolutionCheck sol = new SolutionCheck();
+        String s1 = "()";
+        String s2 = "()[]{}";
+        String s3 = "(]";
+        String s4 = "([])";
+        String s5 = "([)]";
+
+        System.out.println(s1 + " -> " + sol.isValid(s1));
+        System.out.println(s2 + " -> " + sol.isValid(s2));
+        System.out.println(s3 + " -> " + sol.isValid(s3));
+        System.out.println(s4 + " -> " + sol.isValid(s4));
+        System.out.println(s5 + " -> " + sol.isValid(s5));
+    }
+}
