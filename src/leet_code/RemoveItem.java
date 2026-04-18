cfef6f434978bb63c2f28216f8a1da3aac8d79d1
Prasanna B <prasannab806@gmail.com>
2026-04-18 14:10:16 +0530

    Refactor removeElement method in Remove class (8th Problem)

--- src/leet_code/RemoveItem.java
+++ src/leet_code/RemoveItem.java
@@ -2,29 +2,23 @@ package leet_code;
 
 class Remove {
     public int removeElement(int[] nums, int val) {
-        int k = 0; // pointer for valid elements
-
+        int k = 0; 
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] != val) {
                 nums[k] = nums[i];
                 k++;
             }
         }
-
         return k;
     }
 }
-
 public class RemoveItem {
     public static void main(String[] args) {
         Remove obj = new Remove();
         int[] nums = {3, 2, 2, 3};
         int val = 3;
-
         int result = obj.removeElement(nums, val);
-
         System.out.println("Number of elements after removal: " + result);
-
         System.out.print("Updated array: ");
         for (int i = 0; i < result; i++) {
             System.out.print(nums[i] + " ");
