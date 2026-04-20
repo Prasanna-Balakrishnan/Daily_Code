5e62a9ea2c6c6f5abf68a895ef69cffd8cdda5b2
Prasanna-Balakrishnan <prasannab806@gmail.com>
2026-04-20 17:54:58 +0530

    10 Th Problem

---
+++ src/leet_code/MaxDistanceHouses.java
@@ -0,0 +1,38 @@
+package leet_code;
+
+import java.util.*;
+
+public class MaxDistanceHouses {
+    public static int maxDistance(int[] colors) {
+        int n = colors.length;
+        int maxDist = 0;
+        for (int i = n - 1; i >= 0; i--) {
+            if (colors[i] != colors[0]) {
+                maxDist = i;
+                break;
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            if (colors[i] != colors[n - 1]) {
+                maxDist = Math.max(maxDist, (n - 1 - i));
+                break;
+            }
+        }
+        return maxDist;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of houses: ");
+        int n = sc.nextInt();
+        int[] colors = new int[n];
+
+        System.out.println("Enter colors:");
+        for (int i = 0; i < n; i++) {
+            colors[i] = sc.nextInt();
+        }
+        
+        int result = maxDistance(colors);
+        System.out.println("Maximum distance: " + result);
+        sc.close();
+    }
+}
--- src/leet_code/RemoveDuplicate.java
+++ src/leet_code/RemoveDuplicate.java
@@ -5,9 +5,7 @@ import java.util.*;
 class SolutionDuplicate {
     public int removeDuplicates(int[] nums) {
         if (nums.length == 0) return 0;
-
         int k = 1;
-
         for (int i = 1; i < nums.length; i++) {
             if (nums[i] != nums[i - 1]) {
                 nums[k] = nums[i];
@@ -22,13 +20,9 @@ class SolutionDuplicate {
 public class RemoveDuplicate {
     public static void main(String[] args) {
     	SolutionDuplicate obj = new SolutionDuplicate();
-
         int[] nums = {1, 1, 2, 2, 3, 4, 4};
-
         int k = obj.removeDuplicates(nums);
-
         System.out.println("Number of unique elements: " + k);
-
         System.out.print("Array after removing duplicates: ");
         for (int i = 0; i < k; i++) {
             System.out.print(nums[i] + " ");
