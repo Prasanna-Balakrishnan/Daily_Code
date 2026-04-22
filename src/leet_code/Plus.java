0668f9cfac5e3df57725ec5d9a720f99b657924a
Prasanna-Balakrishnan <prasannab806@gmail.com>
2026-04-22 20:37:49 +0530

    12-Th Problem

---
+++ src/leet_code/Plus.java
@@ -0,0 +1,41 @@
+package leet_code;
+public class Plus {
+
+    public int[] plusOne(int[] digits) {
+        int n = digits.length;
+        for (int i = n - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+
+            digits[i] = 0;
+        }
+        int[] result = new int[n + 1];
+        result[0] = 1;
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Solution obj = new Solution();
+        int[] digits1 = {1, 2, 3};
+        int[] digits2 = {4, 3, 2, 1};
+        int[] digits3 = {9};
+
+        printArray(obj.plusOne(digits1)); 
+        printArray(obj.plusOne(digits2)); 
+        printArray(obj.plusOne(digits3)); 
+    }
+    
+
+    public static void printArray(int[] arr) {
+        System.out.print("[");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]);
+            if (i < arr.length - 1) {
+                System.out.print(",");
+            }
+        }
+        System.out.println("]");
+    }
+}
