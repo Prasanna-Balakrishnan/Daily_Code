b090f0a70f178f8dc1ade1458b13009d2f16ea03
Prasanna-Balakrishnan <prasannab806@gmail.com>
2026-04-29 12:20:17 +0530

    19th Problem

---
+++ src/leet_code/NimGame.java
@@ -0,0 +1,16 @@
+package leet_code;
+
+public class NimGame {
+
+    public static boolean canWinNim(int n) {
+        return n % 4 != 0;
+    }
+    public static void main(String[] args) {
+        int n = 5;
+        if (canWinNim(n)) {
+            System.out.println("true");
+        } else {
+            System.out.println("false");
+        }
+    }
+}
