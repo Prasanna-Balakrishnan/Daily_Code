392969cb7ca9eb5bf6fa730290a531b10bedb39b
Prasanna-Balakrishnan <prasannab806@gmail.com>
2026-04-30 13:26:47 +0530

    20-th Problem

---
+++ src/leet_code/FizzBuzz.java
@@ -0,0 +1,28 @@
+package leet_code;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FizzBuzz {
+    public static List<String> fizzBuzz(int n) {
+        ArrayList<String> arr = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                arr.add("FizzBuzz");
+            }
+            else if (i % 3 == 0) {
+                arr.add("Fizz");
+            }
+            else if (i % 5 == 0) {
+                arr.add("Buzz");
+            }
+            else {
+                arr.add(Integer.toString(i));
+            }
+        }
+       return arr;
+    }
+    public static void main(String[] args) {
+        List<String> result = fizzBuzz(15);
+        System.out.println(result);
+    }
+}
