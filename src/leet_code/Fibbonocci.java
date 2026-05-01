0349d13f2d7e67039228f45effdb151c48893307
Prasanna-Balakrishnan <prasannab806@gmail.com>
2026-05-01 18:49:26 +0530

    21st Problem

---
+++ src/leet_code/Fibbonocci.java
@@ -0,0 +1,23 @@
+package leet_code;
+
+public class Fibbonocci {
+    public static int fib(int n) {
+        if(n==1){
+            return 1;
+        }
+        int a=0;
+        int b=1;
+         int c=0;
+        for(int i=2;i<=n;i++){
+           c=a+b;
+            a=b;
+            b=c;
+        }
+        return c;
+    }
+public static void main(String[] args) {
+	int n=1;
+	int result = fib(n);
+	System.out.println(result);
+}
+}
