20cd47709ca1b8c5db08ad3ca7c9dd6795336972
Prasanna-Balakrishnan <prasannab806@gmail.com>
2026-05-02 14:19:31 +0530

    22nd Problem

---
+++ src/leet_code/ClimbingStairs.java
@@ -0,0 +1,27 @@
+package leet_code;
+
+import java.util.Scanner;
+
+public class ClimbingStairs {
+
+    public static int climbStairs(int n) {
+        if (n == 1)
+            return 1;
+        int first = 1;
+        int second = 2;
+
+        for (int i = 3; i <= n; i++) {
+            int third = first + second;
+            first = second;
+            second = third;
+        }
+        return second;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of steps: ");
+        int n = sc.nextInt();
+        System.out.println("Ways to climb: " + climbStairs(n));
+        sc.close();
+    }
+}
