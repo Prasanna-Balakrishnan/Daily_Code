3b56edf562f433fbd18a85da20ff5d7d7f3b0fe6
Prasanna-Balakrishnan <prasannab806@gmail.com>
2026-05-16 10:57:29 +0530

    36th Problem

---
+++ src/leet_code/AddDigits.java
@@ -0,0 +1,33 @@
+package leet_code;
+
+import java.util.Scanner;
+
+public class AddDigits {
+
+    public static int addDigits(int num) {
+        while (num >= 10) {
+            int sum = 0;
+
+            while (num > 0) {
+                sum += num % 10;
+                num = num / 10;
+            }
+
+            num = sum;
+        }
+
+        return num;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int num = sc.nextInt();
+
+        int result = addDigits(num);
+
+        System.out.println(result);
+
+        sc.close();
+    }
+}
