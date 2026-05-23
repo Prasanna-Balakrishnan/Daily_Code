bfb2c82276e20623a4bf081473923412f47d9b23
Prasanna B <prasannab806@gmail.com>
2026-05-23 11:18:44 +0530

    Refactor check method by removing comments
    Removed unnecessary comments for clarity.

--- src/leet_code/SortedAndRotated.java
+++ src/leet_code/SortedAndRotated.java
@@ -5,13 +5,10 @@ public class SortedAndRotated {
         int count = 0;
         int n = nums.length;
         for (int i = 0; i < n; i++) {
-            // Compare current element with next element
-            // % n helps compare last element with first element
             if (nums[i] > nums[(i + 1) % n]) {
                 count++;
             }
         }
-        // Valid only if break count is 0 or 1
         return count <= 1;
     }
     public static void main(String[] args) {
