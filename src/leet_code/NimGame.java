12952715dd2d72d9d652d5b832eb638077005e48
Prasanna B <prasannab806@gmail.com>
2026-06-21 21:28:25 +0530

    Implement canWinNim method in NimGame class

--- src/leet_code/NimGame.java
+++ src/leet_code/NimGame.java
@@ -1,8 +1,10 @@
 package leet_code;
+
 public class NimGame {
     public static boolean canWinNim(int n) {
         return n % 4 != 0;
     }
+    
     public static void main(String[] args) {
         int n = 5;
         if (canWinNim(n)) {
